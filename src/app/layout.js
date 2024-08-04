b5b23052bd9ec963d8cfe474390830e8c6511f3d
adyeetya <2612adityasingh2000@gmail.com>
2024-08-04 20:46:00 +0530

    new

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
+import { cn } from '@/lib/utils'
 import './globals.css'
 import Back from '@/components/footer/Back'
 import ScrollProgressCircle from '@/components/ScrollProgress'
@@ -9,13 +10,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'TripTribe | The Companion of your Journey.',
-  description: 'Websites | Networking | Branding | Marketing | UI/UX | Consulting',
+  description:
+    'Websites | Networking | Branding | Marketing | UI/UX | Consulting',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          'min-h-screen max-w-[100vw] overflow-x-hidden font-sans antialiased scroll-smooth ',
+          inter.className
+        )}
+      >
         <Navbar />
         <ScrollProgressCircle />
         <div className="relative z-10">{children}</div>
